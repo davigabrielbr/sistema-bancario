e09a5db2d9fb7859904bda35c845293cb5db1f50
Davi Gabriel <davi_gabriel99@hotmail.com>
2026-07-31 19:02:43 -0300

    ajustando classe conta

--- src/model/Conta.java
+++ src/model/Conta.java
@@ -4,25 +4,18 @@ public class Conta {
     private double saldo;
 
     public boolean depositar(double valor) {
-        if (valor <= 0) {
-            return false;
-        } else {
-            saldo += valor;
-            return true;
-        }
+        if (valor <= 0) return false;
+
+        saldo += valor;
+        return true;
     }
 
     public boolean sacar(double valor) {
-        if (valor <= 0) {
-            return false;
-        }
+        if (valor <= 0) return false;
+        if (valor > saldo) return false;
 
-        if (valor > saldo) {
-            return false;
-        } else {
-            saldo -= valor;
-            return true;
-        }
+        saldo -= valor;
+        return true;
     }
 
     public double getSaldo() {
